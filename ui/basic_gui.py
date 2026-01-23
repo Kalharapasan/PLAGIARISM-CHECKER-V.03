b6902b540ab76cf32f2eb6c3c92d0e0ce354448a
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-01-23 22:01:08 +0530

    Right panel - Results

--- ui/basic_gui.py
+++ ui/basic_gui.py
@@ -101,4 +101,6 @@ class BasicPlagiarismChecker:
                                      command=self.run_check, cursor='hand2', relief='flat',
                                      activebackground='#38a169', activeforeground='white')
         self.check_button.pack(fill='x', padx=15, pady=(0, 15))
+        right_frame = tk.Frame(main_container, bg='white', relief='raised', bd=1)
+        right_frame.pack(side='right', fill='both', expand=True)
         
