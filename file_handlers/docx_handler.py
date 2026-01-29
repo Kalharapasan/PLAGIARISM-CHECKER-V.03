76a4e79df79d797f7299a89ea63303b4f0e97752
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-01-29 22:20:14 +0530

    Create document

--- file_handlers/docx_handler.py
+++ file_handlers/docx_handler.py
@@ -506,6 +506,7 @@ class DOCXHandler:
             from docx.shared import Inches, Pt, RGBColor
             from docx.enum.text import WD_ALIGN_PARAGRAPH
             from docx.enum.style import WD_STYLE_TYPE
+            doc = Document()
             
 
 
