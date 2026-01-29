880f201c7b80c6d1f338c7f56a20a7886a730a4a
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-01-29 19:01:30 +0530

    Handler for Microsoft Word DOCX files

--- file_handlers/docx_handler.py
+++ file_handlers/docx_handler.py
@@ -8,6 +8,15 @@ import tempfile
 import os
 
 class DOCXHandler:
+    NAMESPACES = {
+        'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main',
+        'wp': 'http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing',
+        'a': 'http://schemas.openxmlformats.org/drawingml/2006/main',
+        'pic': 'http://schemas.openxmlformats.org/drawingml/2006/picture',
+        'r': 'http://schemas.openxmlformats.org/officeDocument/2006/relationships',
+        'mc': 'http://schemas.openxmlformats.org/markup-compatibility/2006',
+        'v': 'urn:schemas-microsoft-com:vml'
+    }
     
 
 
