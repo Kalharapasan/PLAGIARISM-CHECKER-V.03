4dfeb0d63385be75a76b2f72b9606ac8d5dbb571
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-01-30 19:51:50 +0530

    Copy all pages

--- file_handlers/pdf_handler.py
+++ file_handlers/pdf_handler.py
@@ -564,4 +564,6 @@ class PDFHandler:
             with open(filepath, 'rb') as input_file:
                 reader = PdfReader(input_file)
                 writer = PdfWriter()
+                for page in reader.pages:
+                    writer.add_page(page)
                             
