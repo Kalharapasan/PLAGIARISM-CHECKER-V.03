c41d9e494e718a64ca03b4e63406ccd7c9da4b58
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-05 13:19:14 +0530

    Clean up whitespace

--- file_handlers/text_extractor.py
+++ file_handlers/text_extractor.py
@@ -389,3 +389,7 @@ class TextExtractor:
                 for script in soup(["script", "style"]):
                     script.decompose()
                     text = soup.get_text()
+                    lines = (line.strip() for line in text.splitlines())
+                    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+                    text = '\n'.join(chunk for chunk in chunks if chunk)
+                    return text
