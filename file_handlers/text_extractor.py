3559a49788c61e0826106a2ff5cddc16e44e4c50
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-05 13:43:33 +0530

    Remove horizontal rules

--- file_handlers/text_extractor.py
+++ file_handlers/text_extractor.py
@@ -481,4 +481,7 @@ class TextExtractor:
         text = re.sub(r'\[([^\]]+)\]\([^)]+\)', r'\1', text)
         text = re.sub(r'!\[([^\]]*)\]\([^)]+\)', r'\1', text)
         text = re.sub(r'^>\s+', '', text, flags=re.MULTILINE)
+        text = re.sub(r'^[-*_]{3,}\s*$', '', text, flags=re.MULTILINE)
+        
+        return text.strip()
         
