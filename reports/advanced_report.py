7d106d089544668e85b8e252823cea6cc1d62ce5
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-06 22:07:32 +0530

    Generate detailed text report

--- reports/advanced_report.py
+++ reports/advanced_report.py
@@ -1,3 +1,6 @@
 from datetime import datetime
 from typing import Dict, List
 import json
+
+def generate_advanced_report(results: Dict, filename: str, 
+                           algorithms: List[str]) -> str:
