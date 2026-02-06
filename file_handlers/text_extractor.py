0c9091253f6692fe78cfc63153a8dab7f45f6c3d
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-06 21:51:08 +0530

    Could be DOC, XLS, PPT

--- file_handlers/text_extractor.py
+++ file_handlers/text_extractor.py
@@ -798,3 +798,4 @@ def guess_file_format(filepath: str) -> str:
             elif header.startswith(b'PK'):
                 return '.zip'
             elif header.startswith(b'\xD0\xCF\x11\xE0'): 
+                return '.doc'
