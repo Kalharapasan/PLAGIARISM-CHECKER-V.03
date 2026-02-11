e48a6c081899754d05fc5b571039e499d155ee11
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-11 15:01:37 +0530

    Adjust for special cases

--- algorithms/citations.py
+++ algorithms/citations.py
@@ -327,3 +327,7 @@ class TextProcessor:
                 count += 1
             
             previous_was_vowel = is_vowel
+        if word.endswith('e'):
+            count -= 1
+        if word.endswith('le') and len(word) > 2 and word[-3] not in vowels:
+            count += 1
