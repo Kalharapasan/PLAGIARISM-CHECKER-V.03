5ceac1af9185c114858e1a1702cdb35cf2624a0c
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-11 15:07:19 +0530

    Term frequency

--- algorithms/citations.py
+++ algorithms/citations.py
@@ -428,3 +428,5 @@ class TextProcessor:
                     phrase_frequencies[phrase] = 1
         key_phrases = []
         total_phrases = len(phrase_frequencies)
+        for phrase, freq in phrase_frequencies.items():
+            tf = freq / total_phrases if total_phrases > 0 else 0
