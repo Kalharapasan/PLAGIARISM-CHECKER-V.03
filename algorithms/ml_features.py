6e8d82a727e171f270d33fbbdd73a8d2666d0254
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-13 19:36:08 +0530

    First get TF-IDF, then apply SVD

--- algorithms/ml_features.py
+++ algorithms/ml_features.py
@@ -183,3 +183,8 @@ class MLFeatures:
             elif method == 'char':
                 vectorizer = self.vectorizers['char']
                 embedding = vectorizer.fit_transform([text]).toarray()[0]
+            elif method == 'svd':
+                tfidf_vec = self.vectorizers['tfidf']
+                tfidf_matrix = tfidf_vec.fit_transform([text])
+                svd_model = self.models['svd']
+                embedding = svd_model.fit_transform(tfidf_matrix).flatten()
