71e5e352a1b469c0d0873eff0405b4ac7180e872
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-16 13:02:05 +0530

    Sort by position

--- algorithms/nlp.py
+++ algorithms/nlp.py
@@ -551,3 +551,6 @@ class TextProcessor:
                     citation_info['year'] = int(year_match.group())
                 
                 citations.append(citation_info)
+        citations.sort(key=lambda x: x['position'])
+        
+        return citations
