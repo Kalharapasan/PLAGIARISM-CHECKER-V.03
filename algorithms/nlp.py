e9fdc1f5e89772d4322292dcd0d420155178e7c6
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-16 12:50:09 +0530

    Split sentences

--- algorithms/nlp.py
+++ algorithms/nlp.py
@@ -298,4 +298,5 @@ class TextProcessor:
         ]
         for abbr in abbreviations:
             text = text.replace(abbr, abbr.replace('.', '@ABBR@'))
+        sentences = re.split(r'[.!?]+\s+', text)
     
