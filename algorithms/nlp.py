a77cdb71893c7b08813240de65f31c6ad29a7158
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-17 12:41:59 +0530

    Check structure

--- algorithms/nlp.py
+++ algorithms/nlp.py
@@ -778,3 +778,4 @@ class TextAnalyzer:
         academic_words = sum(word_freq[word] for word in self.processor.ACADEMIC_WORDS if word in word_freq)
         academic_ratio = academic_words / len(words) if words else 0
         citations = self.processor.detect_citations(text)
+        structure = self.processor.detect_academic_structure(text)
