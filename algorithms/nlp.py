de52db76dd029bfe16c564252d1d17127d74c847
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-17 12:28:22 +0530

    Count characters (including spaces and punctuation)

--- algorithms/nlp.py
+++ algorithms/nlp.py
@@ -636,5 +636,7 @@ class ReadabilityAnalyzer:
         if not sentences or not words:
             return 0.0
         chars = len(text)
+        avg_chars_per_word = chars / len(words)
+        avg_sentence_length = len(words) / len(sentences)
     
 
