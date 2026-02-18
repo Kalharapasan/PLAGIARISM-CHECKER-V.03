a30932f6b9d2f54bda12cabf9e05fe68efe6a6eb
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-18 21:16:21 +0530

    Academic words

--- algorithms/nlp.py
+++ algorithms/nlp.py
@@ -852,4 +852,6 @@ class TextAnalyzer:
     def _calculate_formality(self, text: str) -> float:
         formal_indicators = {
             'passive': self.processor.detect_passive_voice(text),
+            'academic': sum(1 for word in self.processor.tokenize(text) 
+                          if word in self.processor.ACADEMIC_WORDS),
         }
