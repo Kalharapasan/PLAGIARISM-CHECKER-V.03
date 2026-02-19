564b133f715a8f8c929bca29b8495cb4f23c4438
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-19 12:41:17 +0530

    Create vectors

--- algorithms/similarity.py
+++ algorithms/similarity.py
@@ -188,3 +188,5 @@ class SimilarityCalculator:
         freq1 = Counter(words1)
         freq2 = Counter(words2)
         all_words = set(freq1.keys()).union(set(freq2.keys()))
+        vec1 = [freq1.get(word, 0) for word in all_words]
+        vec2 = [freq2.get(word, 0) for word in all_words]
