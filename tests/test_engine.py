6a9ad0ff193543b84758ad7c43cdbfe3603490d4
Pasan Kalhara <kalharap84@gmail.com>
2026-04-03 23:53:17 +0530

    Add tests for cosine similarity in TestSimilarityAlgorithms

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1190,4 +1190,10 @@ class TestSimilarityAlgorithms(unittest.TestCase):
         """
     
     def test_01_cosine_similarity(self):
+        similarity = self.similarity.calculate_cosine_similarity(
+            self.text1, self.text2
+        )
         
+        self.assertIsInstance(similarity, float)
+        self.assertGreaterEqual(similarity, 0.0)
+        self.assertLessEqual(similarity, 100.0)
